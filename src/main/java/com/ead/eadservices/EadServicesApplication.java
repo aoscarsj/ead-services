712e582b6ebc1a99436b6c0eb53b4073c637678b
Artur Oscar <Arturoscar3@gmail.com>
2022-04-23 16:40:42 -0300

    first commit

---
+++ src/main/java/com/ead/eadservices/EadServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ead.eadservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ead/eadservices/EadServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.eadservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
